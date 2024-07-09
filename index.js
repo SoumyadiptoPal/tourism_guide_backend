8be872e31a042afd2f3527ecace84262ab0d0a7c
SoumyadiptoPal <soumyadiptocodes@gmail.com>
2024-07-10 01:12:33 +0530

    Updated Code

---
+++ index.js
@@ -0,0 +1,28 @@
+// This file is created for the purpose of deploying to vercel. As vercel reqires an index.js file
+
+const express = require('express');
+const connectDB = require('./db');
+const cors = require('cors');
+
+// routes
+const app = express();
+const auth = require('./routes/auth');
+const post = require('./routes/post');
+
+// Connect Database
+connectDB();
+
+//cors
+app.use(cors({ origin: true, credentials: true }));
+
+// Init Middleware
+app.use(express.json());
+app.use(express.urlencoded());
+
+// use Routes
+app.use('/auth', auth);
+app.use('/post', post);
+
+const port = process.env.PORT || 8082;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
